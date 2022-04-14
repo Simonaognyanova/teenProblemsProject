e980c04f37b74da68b7a002e9964002c411439af
Simonaognyanova <simonaognanova05@gmail.com>
2022-04-14 15:08:16 +0300

    teenProblemsProject

---
+++ teenProblems/admin/includes/leftsidebar.php
@@ -0,0 +1,61 @@
+            <div class="left side-menu">
+                <div class="sidebar-inner slimscrollleft">
+
+                    <!--- Sidemenu -->
+                    <div id="sidebar-menu">
+                        <ul>
+                        	<li class="menu-title">Навигация</li>
+
+                            <li class="has_sub">
+                                <a href="dashboard.php" class="waves-effect"><i class="mdi mdi-view-dashboard"></i> <span> Табло </span> </a>
+                            </li>
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Категория </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                	<li><a href="add-category.php">Добавете категория</a></li>
+                                    <li><a href="manage-categories.php">Управление на категории</a></li>
+                                </ul>
+                            </li>
+
+    <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span>Подкатегория </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-subcategory.php">Добавете подкатегория</a></li>
+                                    <li><a href="manage-subcategories.php">Управление на подкатегория</a></li>
+                                </ul>
+                            </li>               
+  <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Публикации </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="add-post.php">Добавете публикация</a></li>
+                                    <li><a href="manage-posts.php">Управление на публикации</a></li>
+                                     <li><a href="trash-posts.php">Изтрити публикации</a></li>
+                                </ul>
+                            </li>  
+                     
+
+                            <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Страници </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                    <li><a href="aboutus.php">За нас</a></li>
+                                    <li><a href="contactus.php">Контакти</a></li>
+                                </ul>
+                            </li>
+   <li class="has_sub">
+                                <a href="javascript:void(0);" class="waves-effect"><i class="mdi mdi-format-list-bulleted"></i> <span> Коментари </span> <span class="menu-arrow"></span></a>
+                                <ul class="list-unstyled">
+                                  <li><a href="unapprove-comment.php">В очакване на одобрение... </a></li>
+                                    <li><a href="manage-comments.php">Одобрени коментари</a></li>
+                                </ul>
+                            </li>   
+                        </ul>
+                    </div>
+                    <!-- Sidebar -->
+                    <div class="clearfix"></div>
+                    <div class="help-box">
+                        <h5 class="text-muted m-t-0">Нужда от помощ ?</h5>
+                        <p class=""><span class="text-custom">Имейл:</span> <br/> simonaognanova05@gmail.com</p>
+                    </div>
+                </div>
+                <!-- Sidebar -left -->
+            </div>
---
+++ teenProblems/mail/contact_me.php
@@ -0,0 +1,26 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'simonaognanova05@gmail.com'; 
+$email_subject = "Форма за контакт на уебсайта:  $name";
+$email_body = "Получихте ново съобщение от формата за контакт на вашия уебсайт.\n\n"."Ето подробностите:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
